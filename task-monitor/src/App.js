2f24a4eeaa1a82a61da108f77d3da6dfa13c97df
stevescilar <stevemuambi@gmail.com>
2022-02-15 20:55:50 +0300

    progress 0-1

--- task-monitor/src/App.js
+++ task-monitor/src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome from react</h1>
+    <div className="container">
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ task-monitor/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return(
+  <header>
+      <h1>Task Monitor</h1>
+  </header>
+  )
+}
+
+export default Header
